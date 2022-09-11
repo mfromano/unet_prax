7d625862079dda686da57471760a2c64df635557
Mike <mfromano@pm.me>
2022-09-10 20:18:09 -0700

    completed unet

--- main.py
+++ main.py
@@ -3,42 +3,36 @@ from unet_prax.src.models import *
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+if __name__ == '__main__':
+    writer = SummaryWriter()
 
-writer = SummaryWriter()
+    net = UNet(channel_list=(1,12,24,48,96,192,))
 
-net = UNet(channel_list=(1,12,24,48,96,192,))
+    DEVICE = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
 
-DEVICE = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
+    net.to(DEVICE)
 
-net.to(DEVICE)
+    loss_fun = nn.BCEWithLogitsLoss().to(DEVICE)
+    optim = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+    optim.zero_grad()
 
-loss_fun = nn.BCEWithLogitsLoss().to(DEVICE)
-optim = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
-optim.zero_grad()
+    train_data = MnistData(True)
+    test_data = MnistData(False)
 
-train_data = MnistData(True)
-test_data = MnistData(False)
+    train_dataloader = DataLoader(train_data, batch_size=25, drop_last=True)
+    test_dataloader = DataLoader(test_data, batch_size=len(test_data))
 
-train_dataloader = DataLoader(train_data, batch_size=25, drop_last=True)
-test_dataloader = DataLoader(test_data, batch_size=len(test_data))
-
-# for samples, labels in train_dataloader:
-#     grid = torchvision.utils.make_grid(labels)
-#     plt.imshow(grid.permute(1,2,0))
-#     break
-
-
-def train_model(n_epochs):
-    for epoch in range(n_epochs):
-        print(f'Epoch # {epoch}')
-        for samples, labels in tqdm(train_dataloader):
-            samples, labels = samples.to(DEVICE), labels.to(DEVICE)
-            optim.zero_grad()
-            out = net(samples)
-            loss = loss_fun(out, transforms.CenterCrop(out.shape[2:3])(samples))
-            writer.add_scalar('Loss/train', loss.item(), epoch)
-            loss.backward()
-            optim.step()
-    
-train_model(20)
-writer.flush()
+    def train_model(n_epochs):
+        for epoch in range(n_epochs):
+            print(f'Epoch # {epoch}')
+            for samples, labels in tqdm(train_dataloader):
+                samples, labels = samples.to(DEVICE), labels.to(DEVICE)
+                optim.zero_grad()
+                out = net(samples)
+                loss = loss_fun(out, transforms.CenterCrop(out.shape[2:3])(samples))
+                writer.add_scalar('Loss/train', loss.item(), epoch)
+                loss.backward()
+                optim.step()
+        
+    train_model(20)
+    writer.flush()
