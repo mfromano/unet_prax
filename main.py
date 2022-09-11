4dc4d9cefcf0ecff2a0217bfe55173afad1ef82f
mfromano <michaelfromano@gmail.com>
2022-09-11 11:35:23 -0700

    ready to run

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
 
     net = UNet(channel_list=(1,12,24,48,96,192,))
 
-    DEVICE = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
+    if torch.backends.mps.is_available():
+        DEVICE = torch.device('mps')
+    elif torch.cuda.is_available():
+        DEVICE = torch.device('cuda:0')
+    else:
+        DEVICE = torch.device('cpu')
 
     net.to(DEVICE)
 
@@ -24,7 +29,10 @@ if __name__ == '__main__':
     train_dataloader = DataLoader(train_data, batch_size=25, drop_last=True)
     test_dataloader = DataLoader(test_data, batch_size=len(test_data))
 
+    
     def train_model(n_epochs):
+        min_loss = np.inf
+        step = 0
         for epoch in range(n_epochs):
             print(f'Epoch # {epoch}')
             for samples, labels in tqdm(train_dataloader):
@@ -32,15 +40,19 @@ if __name__ == '__main__':
                 optim.zero_grad()
                 out = net(samples)
                 loss = loss_fun(out, transforms.CenterCrop(out.shape[2:3])(samples))
-                writer.add_scalar('Loss/train', loss.item(), epoch)
+                step += 1
+                writer.add_scalar('Loss/train', loss.item(), step)
                 loss.backward()
                 optim.step()
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': net.state_dict(),
-            'optimizer_state_dict': optim.state_dict(),
-            'loss': loss,
-            }, '.')
+            if loss.item() < min_loss:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': net.state_dict(),
+                    'optimizer_state_dict': optim.state_dict(),
+                    'loss': loss,
+                }, 'model.pth')
+                min_loss = loss.item()
+        
             
     train_model(20)
     writer.flush()
